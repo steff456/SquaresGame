006af3a0236f5462dafdab3abde0d092c8fb2a7e
steff456 <steff456@hotmail.com>
2018-10-02 23:12:47 -0500

    Header done

--- imports/ui/containers/Header/index.js
+++ imports/ui/containers/Header/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+
 import MainNavBar from './MainNavBar';
 
 export default class Header extends React.Component {
   render() {
-    return (
-      <header>
-        <Container className="header-container d-flex justify-content-between align-items-stretch">
-          <MainNavBar />
-        </Container>
-      </header>
-    );
+    return <MainNavBar />;
   }
 }
