c4f924068c101ee7f79fedc4ce04811daf30ceff
steff456 <steff456@hotmail.com>
2018-10-04 10:59:50 -0500

    init boxes, edges, line

--- imports/ui/components/Game/index.js
+++ imports/ui/components/Game/index.js
@@ -1,23 +1,51 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
-import { Games } from '../../../api/games.js';
 
+import { Games } from '../../../api/games.js';
+import { Match } from '../../../api/match.js';
 
 export default class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.leaveGame = this.leaveGame.bind(this);
+    this.getCurrentGame = this.getCurrentGame.bind(this);
+  }
 
-    constructor(props) {
-        super(props)
-        this.leaveGame = this.leaveGame.bind(this);
-    }
+  state = {
+    turn: 'p1',
+    start: False,
+  };
 
-    leaveGame(){   
-        Games.remove(this.props.id);
-        this.props.onGame(null);
-    }
+  componentDidMount() {
+    console.log(this.props);
+    this.getCurrentGame();
+  }
 
-    render() {
-        return <div>
-            <Button onClick={this.leaveGame}>Leave the game</Button>
-        </div>
+  componentDidUpdate() {
+    console.log('******');
+    this.getPlayers();
+  }
+
+  getCurrentGame() {
+    const { id } = this.props;
+    const g = Games.find({ id: { $ne: id } }).fetch();
+    console.log(g[g.length - 1]);
+    if (g.player2_id !== null) {
+      // Start game
     }
+  }
+
+  leaveGame() {
+    const { id } = this.props;
+    Games.remove(id);
+    this.props.onGame(null);
+  }
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.leaveGame}>Leave the game</Button>
+      </div>
+    );
+  }
 }
--- imports/ui/components/Lobby/index.js
+++ imports/ui/components/Lobby/index.js
@@ -1,84 +1,89 @@
 import React from 'react';
-import { Games } from '../../../api/games.js';
-import { withTracker } from 'meteor/react-meteor-data'
-import PropTypes from 'prop-types'
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
 import { Button } from 'semantic-ui-react';
-import { Meteor } from 'meteor/meteor'
+import { Meteor } from 'meteor/meteor';
+import { Games } from '../../../api/games.js';
 
 class Lobby extends React.Component {
+  constructor(props) {
+    super(props);
+    this.joinGame = this.joinGame.bind(this);
+    this.changeGame = this.changeGame.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.joinGame = this.joinGame.bind(this);
-        this.changeGame = this.changeGame.bind(this);
+  renderActiveGames() {
+    return this.props.games.map((g, i) => (
+      <li key={i}>
+        {g.id}
+{' '}
+-
+{' '}
+{g.status}
+        <Button key={g.id} onClick={() => this.joinGame(g.id)}>
+          {' '}
+          Join
+{" "}
+        </Button>
+      </li>
+    ));
+  }
+
+  joinGame(id) {
+    const game = Games.find({ id }).fetch();
+    Games.update(game[0]._id, {
+      status: 'playing',
+      player2_id: Meteor.userId(),
+      player2_user: Meteor.user().username,
+    });
+    this.changeGame(game[0]._id);
+  }
+
+  changeGame(id) {
+    this.props.onGame(id);
+  }
+
+  createNewGame(evt) {
+    evt.preventDefault();
+
+    const exist = Games.find({ id: Meteor.userId() }).fetch();
+
+    if (exist.length != 0) {
+      alert('You have already a game');
+      this.changeGame(exist[0]._id);
+    } else {
+      alert('new game created');
+      const newGame = Games.insert({
+        id: Meteor.userId(),
+        status: 'waiting',
+        player1_id: Meteor.userId(),
+        player1_user: Meteor.user().username,
+        player2_id: null,
+        player2_user: null,
+        date: new Date(),
+      });
+      this.changeGame(newGame);
     }
+  }
 
-    renderActiveGames() {
-        return this.props.games.map((g, i) => {
-            return (<li key={i}>
-                {g.id} - {g.status}
-                <Button key={g.id} onClick={() => this.joinGame(g.id)}> Join </Button>
-            </li>);
-        });
-    }
-
-    joinGame(id) {
-        let game = Games.find({ id: id }).fetch();
-        Games.update(game[0]._id,
-            {
-                status: "playing",
-                player2_id: Meteor.userId(),
-                player2_user: Meteor.user().username
-            });
-        this.changeGame(game[0]._id);
-    }
-
-    changeGame(id) {
-        this.props.onGame(id);
-    }
-
-    createNewGame(evt) {
-        evt.preventDefault();
-
-        let exist = Games.find({ id: Meteor.userId() }).fetch();
-
-        if (exist.length != 0) {
-            alert("You have already a game");
-            this.changeGame(exist[0]._id);
-        }
-        else {
-            alert("new game created");
-            let newGame = Games.insert({
-                id: Meteor.userId(),
-                status: "waiting",
-                player1_id: Meteor.userId(),
-                player1_user: Meteor.user().username,
-                player2_id: null,
-                player2_user:null,
-                date: new Date()
-            });
-            this.changeGame(newGame);
-        }
-    }
-
-
-    render() {
-        return <div>
-            <Button onClick={this.createNewGame.bind(this)}>Create new Game</Button>
-            <ul>
-                {this.renderActiveGames()}
-            </ul>
-        </div>
-    }
+  render() {
+    return (
+      <div>
+        <Button onClick={this.createNewGame.bind(this)}>Create new Game</Button>
+        <ul>{this.renderActiveGames()}</ul>
+      </div>
+    );
+  }
 }
 
 Lobby.propTypes = {
-    games: PropTypes.array.isRequired,
-    onGame: PropTypes.func
-}
+  games: PropTypes.array.isRequired,
+  onGame: PropTypes.func,
+};
 
-export default withTracker(() => {
-    return {
-        games: Games.find({ status: "waiting", id: { $ne: Meteor.userId() } }).fetch()
-    };
-})(Lobby);
+export default withTracker(() => ({
+  games: Games.find({
+    status: 'waiting',
+    id: { $ne: Meteor.userId() },
+  }).fetch(),
+}))(Lobby);
